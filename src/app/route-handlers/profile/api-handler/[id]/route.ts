cfad37865fc93f2a6bc9d7945ecd39cc908de6f0
geekAshish <ashishkushwaha472004@gmail.com>
2024-01-26 17:40:28 +0530

    Handling PATCH request

--- src/app/route-handlers/profile/api-handler/[id]/route.ts
+++ src/app/route-handlers/profile/api-handler/[id]/route.ts
@@ -5,3 +5,15 @@ export async function GET(request: Request, {params}: {params: {id: string}}) {
   const user = userDetails.find(user => user.id === parseInt(params.id))
   return Response.json(user);
 }
+
+
+export async function PATCH(request: Request, {params}: {params: {id: string}}) {
+  const body = await request.json();
+  const {name} = body;
+
+  const index = userDetails.findIndex(user => user.id === parseInt(params.id));
+  userDetails[index].name = name;
+
+  return Response.json(userDetails[index]);
+}
+
